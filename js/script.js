4bdd65f2cd9b1336b931d09a456447fdb03ff8d1
PatMan10 <PatMan10@protonmail.com>
2019-07-16 18:24:25 +0200

    nav-drawer added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 function myClick() {
   alert('Click')
 }
+
+function openNavDrawer() {
+  const navD = document.getElementById('nav-drawer')
+  navD.style.setProperty('left', '0')
+}
+
+function closeNavDrawer() {
+  const navD = document.getElementById('nav-drawer')
+  navD.style.setProperty('left', '-250px')
+}
