a71d6dd35ebff0972dfe3404ea92a5343228c8de
PatMan10 <PatMan10@protonmail.com>
2019-07-18 19:35:42 +0200

    fixed id navigation (compensated for header)

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-function myClick() {
-  alert('Click')
+function zol() {
+  console.log(window.pageYOffset)
+}
+
+function adjustYOffset() {
+  setTimeout(() => {
+    const diff = 91
+    const curPos = window.pageYOffset
+    if (curPos < diff) return
+    const newPos = curPos - diff
+    window.scrollTo(0, newPos);
+  }, 5)
 }
 
 function openNavDrawer() {
